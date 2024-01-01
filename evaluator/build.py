d7eea701aa99d03d315897afbb7c7d05f7d7780c
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2024-01-01 10:39:53 +0900

    Update build.py

--- evaluator/build.py
+++ evaluator/build.py
@@ -4,7 +4,7 @@ from evaluator.voc_evaluator import VOCAPIEvaluator
 
 def build_evluator(args, data_cfg, transform, device):
     # Basic parameters
-    data_dir = os.path.join(args.root, data_cfg['data_name'])
+    data_dir = os.path.join(args.data_path, data_cfg['data_name'])
 
     # Evaluator
     ## VOC Evaluator
