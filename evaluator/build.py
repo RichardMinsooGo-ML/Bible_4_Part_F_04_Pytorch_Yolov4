550699ef7f76ef39840c3ca791e0a0f1598fec4f
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2024-01-01 12:59:59 +0900

    Add files via upload

--- evaluator/build.py
+++ evaluator/build.py
@@ -1,10 +1,15 @@
 import os
 
+from evaluator.coco_evaluator import COCOAPIEvaluator
 from evaluator.voc_evaluator import VOCAPIEvaluator
+from evaluator.crowdhuman_evaluator import CrowdHumanEvaluator
+from evaluator.ourdataset_evaluator import OurDatasetEvaluator
+
+
 
 def build_evluator(args, data_cfg, transform, device):
     # Basic parameters
-    data_dir = os.path.join(args.data_path, data_cfg['data_name'])
+    data_dir = os.path.join(args.root, data_cfg['data_name'])
 
     # Evaluator
     ## VOC Evaluator
@@ -13,4 +18,11 @@ def build_evluator(args, data_cfg, transform, device):
                                     device    = device,
                                     transform = transform
                                     )
+    ## COCO Evaluator
+    elif args.dataset == 'coco':
+        evaluator = COCOAPIEvaluator(data_dir  = data_dir,
+                                     device    = device,
+                                     transform = transform
+                                     )
+
     return evaluator
